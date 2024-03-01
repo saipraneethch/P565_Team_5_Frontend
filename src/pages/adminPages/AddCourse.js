375b1f427f00f4997e912dad660e2bcaedcc4efc
Prithvi <prithvia28@gmail.com>
2024-03-01 17:00:32 -0500

    add new course

--- src/pages/adminPages/AddCourse.js
+++ src/pages/adminPages/AddCourse.js
@@ -1,69 +1,163 @@
-import React, { useState } from 'react';
-import '../../index.css';
-import '../../styles/AddCourse.css'; // Import the CSS styles
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../../index.css";
+import "../../styles/AddCourse.css"; // Import the CSS styles
 
-import Select from 'react-select';
+import Select from "react-select";
 
-//import 'react-select/dist/react-select.css';
-
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCoursesContext } from "../../hooks/useCoursesContext";
 
 const AddCourse = () => {
+  const { dispatch } = useCoursesContext();
+  const { user } = useAuthContext();
+  const navigate = useNavigate();
+
   const [course, setCourse] = useState({
-    code: '',
-    title: '',
-    description: '',
+    code: "",
+    title: "",
+    description: "",
     category: [],
-    instructor: '',
-    startDate: '',
-    endDate: '',
-    bibliography: []
+    instructor: "",
+    start_date: "",
+    end_date: "",
+    bibliography: [{ title: "", author: "" }],
   });
 
+  const [instructors, setInstructors] = useState([]);
+  const [selectedInstructor, setSelectedInstructor] = useState(null);
+  const [error, setError] = useState(null);
+
   const categoryOptions = [
-    { value: 'business', label: 'Business' },
-    { value: 'engineering', label: 'Engineering' },
-    { value: 'arts', label: 'Arts' },
-    { value: 'music', label: 'Music' },
-    { value: 'computer science', label: 'Computer Science' },
-    { value: 'data science', label: 'Data Science' },
+    { value: "business", label: "Business" },
+    { value: "engineering", label: "Engineering" },
+    { value: "arts", label: "Arts" },
+    { value: "music", label: "Music" },
+    { value: "computer science", label: "Computer Science" },
+    { value: "data science", label: "Data Science" },
     // ... add other categories
   ];
 
   const handleCategoryChange = (selectedOptions) => {
-    setCourse(prevCourse => ({
+    setCourse((prevCourse) => ({
       ...prevCourse,
-      category: selectedOptions ? selectedOptions.map(option => option.value) : []
+      category: selectedOptions
+        ? selectedOptions.map((option) => option.value)
+        : [],
     }));
   };
 
+  const handleInstructorChange = (selectedOption) => {
+    setCourse({
+      ...course,
+      instructor: selectedOption ? selectedOption.value : "",
+    });
+    setSelectedInstructor(selectedOption);
+  };
+
+  const handleBibliographyChange = (index, field, value) => {
+    const newBibliography = [...course.bibliography];
+    newBibliography[index][field] = value;
+    setCourse({ ...course, bibliography: newBibliography });
+  };
+
+  const addBibliographyField = () => {
+    setCourse({
+      ...course,
+      bibliography: [...course.bibliography, { title: "", author: "" }],
+    });
+  };
+
+  const removeBibliographyField = (index) => {
+    const newBibliography = [...course.bibliography];
+    newBibliography.splice(index, 1);
+    setCourse({ ...course, bibliography: newBibliography });
+  };
 
   const handleChange = (e) => {
-    
-      //setCourse(prevCourse => ({ ...prevCourse, [name]: value }));
-    
+    const { name, value } = e.target;
+    setCourse((prevCourse) => ({
+      ...prevCourse,
+      [name]: value,
+    }));
   };
- 
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Logic to send course data to the server
-    console.log(course);
 
-    await fetch('api/v1/coursedetails/add-course', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(course),
+    const response = await fetch("api/v1/coursedetails/add-course", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(course),
+    });
+    const json = await response.json();
+    if (!response.ok) {
+      setError(json.error);
+    } else {
+      setCourse({
+        code: "",
+        title: "",
+        description: "",
+        category: [],
+        instructor: "",
+        start_date: "",
+        end_date: "",
+        bibliography: [{ title: "", author: "" }],
+      });
+      setError(null);
+      setSelectedInstructor(null); 
+      dispatch({ type: "CREATE_COURSE", payload: json });
+    }
+  };
+
+  // Fetch instructors on component mount
+  useEffect(() => {
+    const fetchInstructors = async () => {
+      try {
+        const response = await fetch("/api/v1/coursedetails/get-instructors", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
         });
+
+        if (!response.ok) {
+          const errorData = await response.text();
+          throw new Error(`Failed to fetch instructors: ${errorData}`);
+        }
+
+        const data = await response.json();
+        const instructorOptions = data.map((instructor) => ({
+          value: instructor._id,
+          label: `${instructor.first_name} ${instructor.last_name}`,
+        }));
+        setInstructors(instructorOptions);
+      } catch (error) {
+        console.error("Failed to fetch instructors:", error);
+      }
+    };
+
+    if (user && user.token) {
+      fetchInstructors();
+    }
+  }, [user, dispatch]);
+
+  const handleBack = () => {
+    navigate(-1);
   };
 
   return (
     <div className="main-content">
       <div className="form-container">
+        {/* Back button */}
+        <button type="button" onClick={handleBack}>
+          Back
+        </button>
         <h2>Add New Course</h2>
         <form className="form" onSubmit={handleSubmit}>
-          {/* Course Code */}
+          {error && <div className="error">{error}</div>}
           <div className="form-group">
             <label htmlFor="code">Course Code:</label>
             <input
@@ -74,7 +168,6 @@ const AddCourse = () => {
               onChange={handleChange}
             />
           </div>
-          {/* Title */}
           <div className="form-group">
             <label htmlFor="title">Title:</label>
             <input
@@ -85,7 +178,6 @@ const AddCourse = () => {
               onChange={handleChange}
             />
           </div>
-          {/* Description */}
           <div className="form-group">
             <label htmlFor="description">Description:</label>
             <textarea
@@ -95,33 +187,32 @@ const AddCourse = () => {
               onChange={handleChange}
             />
           </div>
-          {/* Category */}
           <div className="form-group">
-  <label htmlFor="category">Category:</label>
-  <Select
-        isMulti
-        name="category"
-        options={categoryOptions}
-        className="basic-multi-select"
-        classNamePrefix="select"
-        onChange={handleCategoryChange}
-        value={categoryOptions.filter(option => course.category.includes(option.value))}
-      />
-    
-    
-</div>
-          {/* Instructor ID */}
-          <div className="form-group">
-            <label htmlFor="instructor">Instructor:</label>
-            <input
-              id="instructor"
-              name="instructor"
-              type="text"
-              value={course.instructor}
-              onChange={handleChange}
+            <label htmlFor="category">Category:</label>
+            <Select
+              isMulti
+              name="category"
+              options={categoryOptions}
+              className="basic-multi-select"
+              classNamePrefix="select"
+              onChange={handleCategoryChange}
+              value={categoryOptions.filter((option) =>
+                course.category.includes(option.value)
+              )}
             />
           </div>
-          {/* Start Date */}
+          <div className="form-group">
+            <label htmlFor="instructor">Instructor:</label>
+            <Select
+              id="instructor"
+              options={instructors}
+              onChange={handleInstructorChange}
+              value={selectedInstructor}
+              isClearable={true}
+              isSearchable={true}
+              placeholder="Select an instructor"
+            />
+          </div>
           <div className="form-group">
             <label htmlFor="start_date">Start Date:</label>
             <input
@@ -132,7 +223,6 @@ const AddCourse = () => {
               onChange={handleChange}
             />
           </div>
-          {/* End Date */}
           <div className="form-group">
             <label htmlFor="end_date">End Date:</label>
             <input
@@ -143,19 +233,45 @@ const AddCourse = () => {
               onChange={handleChange}
             />
           </div>
-          {/* Bibliography */}
-          <div className="form-group">
-            <label htmlFor="bibliography">Bibliography:</label>
-            <input
-              id="bibliography"
-              name="bibliography"
-              type="text"
-              value={course.bibliography}
-              onChange={handleChange}
-              placeholder="Separate items with commas"
-            />
-          </div>
-          {/* Submit button */}
+          <label>Bibliography:</label>
+          {course.bibliography.map((bibEntry, index) => (
+            <div className="bibliography-group" key={index}>
+              <div className="form-group">
+                <label htmlFor={`title-${index}`}>Title:</label>
+                <input
+                  id={`title-${index}`}
+                  name={`title-${index}`}
+                  type="text"
+                  value={bibEntry.title}
+                  onChange={(e) =>
+                    handleBibliographyChange(index, "title", e.target.value)
+                  }
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor={`author-${index}`}>Author:</label>
+                <input
+                  id={`author-${index}`}
+                  name={`author-${index}`}
+                  type="text"
+                  value={bibEntry.author}
+                  onChange={(e) =>
+                    handleBibliographyChange(index, "author", e.target.value)
+                  }
+                />
+              </div>
+              <button
+                type="button"
+                onClick={() => removeBibliographyField(index)}
+              >
+                Remove
+              </button>
+              <button type="button" onClick={addBibliographyField}>
+                Add Another Bibliography
+              </button>
+            </div>
+          ))}
+
           <button type="submit" className="button">
             Submit
           </button>
--- src/pages/adminPages/CourseDetails.js
+++ src/pages/adminPages/CourseDetails.js
@@ -1,19 +1,36 @@
-import React, { useEffect } from "react";
-// import "../../styles/HomePage.css"; // Import CSS for styling
+import React, { useEffect, useState } from "react";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { Link } from "react-router-dom";
 import { useCoursesContext } from "../../hooks/useCoursesContext";
 
-// import formatDistanceToNow from 'date-fns/formatDistanceToNow';
-// import { format } from 'date-fns';
-
 import '../../styles/CourseDetails.css'
 
-
 const CourseDetail = ({ coursedetail }) => {
-  // const { dispatch } = useCoursesContext();
-  // const { user } = useAuthContext();
-  console.log("Single course",coursedetail)
+  const [instructorName, setInstructorName] = useState(""); // State to store instructor name
+  const { user } = useAuthContext();
+
+  useEffect(() => {
+    const fetchInstructorName = async () => {
+      try {
+        const response = await fetch(`/api/v1/coursedetails/get-single-instructor/${coursedetail.instructor}`, {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        });
+        const data = await response.json();
+        if (response.ok) {
+          setInstructorName(`${data.first_name} ${data.last_name}`);
+        }
+      } catch (error) {
+        console.error("Failed to fetch instructor details:", error);
+        setInstructorName("Unknown");
+      }
+    };
+
+    if (user) {
+      fetchInstructorName();
+    }
+  }, [coursedetail.instructor, user]);
 
   const formatDate = (dateString) => {
     return new Date(dateString).toLocaleDateString(undefined, {
@@ -22,7 +39,6 @@ const CourseDetail = ({ coursedetail }) => {
       day: 'numeric'
     });
   };
- 
 
   return (
     <div className="course-details">
@@ -30,39 +46,19 @@ const CourseDetail = ({ coursedetail }) => {
         <h4>{coursedetail.code}: {coursedetail.title}</h4>
         <p><strong>Description: </strong>{coursedetail.description}</p>
         <p><strong>Category: </strong>{coursedetail.category.join(', ')}</p> {/* Assuming category is an array of strings */}
-        <p><strong>Instructor: </strong>{coursedetail.instructor}</p>
+        <p><strong>Instructor: </strong>{instructorName}</p> {/* Display instructor name */}
         <p><strong>Start Date: </strong>{formatDate(coursedetail.start_date)}</p>
         <p><strong>End Date: </strong>{formatDate(coursedetail.end_date)}</p>
-        <p><strong>Bibliography: </strong></p>
-        
-        <ul>
-          {coursedetail.bibliography.map((book, index) => (
-            <li key={index}>{`${book[0]} by ${book[1]}`}</li> // Adjust depending on your array structure
-          ))}
-        </ul>
-        {/* <p>{formatDistanceToNow(new Date(coursedetail.createdAt), { addSuffix: true })}</p> */}
       </div>
     </div>
   );
 }
 
-
 const CourseDetails = () => {
-
   const { courses, dispatch } = useCoursesContext();
   const { user } = useAuthContext();
 
-  console.log("Course details");
-  console.log(courses)
-
-  // Conditional console log to avoid undefined errors
-  if (user) {
-    console.log(user.username); // Assuming the username is directly on the user object
-  } else {
-    console.log("User is not defined.");
-  }
-
-  useEffect(()=>{
+  useEffect(() => {
     const fetchCourses = async () => {
       try {
         const response = await fetch("/api/v1/coursedetails", {
@@ -71,20 +67,18 @@ const CourseDetails = () => {
           },
         });
         const json = await response.json();
-        console.log(json)
-
         if (response.ok) {
           dispatch({ type: "SET_COURSES", payload: json });
         }
       } catch (error) {
         console.error("Failed to fetch course details:", error);
       }
-    }
+    };
 
-    if (user){
-      fetchCourses()
+    if (user) {
+      fetchCourses();
     }
-  },[dispatch,user])
+  }, [dispatch, user]);
 
   return (
     <div className="course-container">
@@ -93,16 +87,12 @@ const CourseDetails = () => {
         <Link to="/add-course">Add a new Course</Link>
       </div>
       <div className="courses-wrapper"> 
-      <div className="courses">
-        
-        {courses &&
-          courses.map((coursedetail) => (
+        <div className="courses">
+          {courses && courses.map((coursedetail) => (
             <CourseDetail key={coursedetail._id} coursedetail={coursedetail} />
           ))}
+        </div>
       </div>
-      {/* For debugging: */}
-    {/* <pre>{JSON.stringify(users, null, 2)}</pre> This will print the updated `userdetails` */}
-  </div>
     </div>
   );
 };
