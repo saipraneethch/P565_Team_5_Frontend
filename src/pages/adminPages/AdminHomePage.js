5c7299a26629835e1a38e1ddbdd9be76513cddcd
Prithvi <prithvia28@gmail.com>
2024-04-09 21:12:34 -0400

    merge conflicts

---
+++ src/pages/adminPages/AdminHomePage.js
@@ -0,0 +1,15 @@
+// First, import React and other dependencies
+import React from 'react';
+
+const AdminHomePage = () => {
+  // Data for the histogram (operating systems)
+  
+  
+  return (
+    <div className="dashboard-container">
+     
+    </div>
+  );
+};
+
+export default AdminHomePage;
