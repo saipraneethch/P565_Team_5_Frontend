95593f142cdec634e9ac20e542bf0d1dc794c61f
Prithvi <prithvia28@gmail.com>
2024-04-22 15:56:43 -0400

    css

--- tests/topNavbar.test.js
+++ tests/topNavbar.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import Navbar from '../src/components/topNavbar'; 
 import { useAuthContext } from '../src/hooks/useAuthContext'; 
 import { useLogout } from '../src/hooks/useLogout'; 
-
 
 jest.mock('../src/hooks/useAuthContext');
 jest.mock('../src/hooks/useLogout');
@@ -13,36 +12,31 @@ describe('Navbar', () => {
     const mockLogout = jest.fn();
 
     beforeEach(() => {
-        
         mockLogout.mockClear();
         useLogout.mockReturnValue({ logout: mockLogout });
     });
 
     test('renders login and register links when no user is logged in', () => {
         useAuthContext.mockReturnValue({ user: null });
-    
-        const { getByText } = render(<Router><Navbar /></Router>);
-        const loginLink = getByText(/login/i); // Case-insensitive match
-        const registerLink = getByText(/register/i); // Case-insensitive match
-    
-        expect(loginLink).toBeInTheDocument();
-        expect(registerLink).toBeInTheDocument();
+
+        render(<Router><Navbar /></Router>);
+        expect(screen.getByText(/login/i)).toBeInTheDocument();
+        expect(screen.getByText(/register/i)).toBeInTheDocument();
     });
-    
 
     test('renders user info and logout button when user is logged in', () => {
         useAuthContext.mockReturnValue({ user: { username: 'testuser' } });
 
-        const { getByText } = render(<Router><Navbar /></Router>);
-        expect(getByText('testuser')).toBeInTheDocument();
-        expect(getByText('Logout')).toBeInTheDocument();
+        render(<Router><Navbar /></Router>);
+        expect(screen.getByText(/testuser/i)).toBeInTheDocument();
+        expect(screen.getByText(/logout/i)).toBeInTheDocument();
     });
 
     test('calls logout function when logout button is clicked', () => {
         useAuthContext.mockReturnValue({ user: { username: 'testuser' } });
 
-        const { getByText } = render(<Router><Navbar /></Router>);
-        const logoutButton = getByText('Logout');
+        render(<Router><Navbar /></Router>);
+        const logoutButton = screen.getByText(/logout/i);
         fireEvent.click(logoutButton);
         expect(mockLogout).toHaveBeenCalled();
     });
